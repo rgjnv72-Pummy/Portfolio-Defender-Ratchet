df139ee05e1930b892470139a793f47e19dcf7d0
rgjnv72-Pummy <rgjnv72@gmail.com>
2026-06-13 08:04:21 +0530

    Add VSA-HMM regime classification scanner and Friday evening workflow

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python -m pip install --upgrade pip
-pip install yfinance pandas==1.5.3 numpy==1.26.4 tqdm scipy scikit-learn
+pip install yfinance pandas==1.5.3 numpy==1.26.4 tqdm scipy scikit-learn hmmlearn
 pip install pandas-ta==0.4.71b0
